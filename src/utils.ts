4ea7186b6e7e16436974a758e9c9be34869d6ff3
wormjim <gcooled29@gmail.com>
2021-05-05 16:44:01 -0400

    Clean Up Utilz

--- src/utils.ts
+++ src/utils.ts
@@ -1,26 +1,20 @@
-import path from 'path';
-import fs from 'fs';
+import * as path from 'path';
+import * as fs from 'fs';
 
 export const getPackage = (workspace: string): { version: string } => {
-  const pathToPackage = path.join(workspace, 'package.json');
+  const pathToPackage = path.resolve(workspace, 'package.json');
   if (!fs.existsSync(pathToPackage)) throw new Error("package.json could not be found in your project's root.");
   return require(pathToPackage);
 };
 
-export const messagesSome = (words: string[], regex?: RegExp) => {
-  return (cb: (...args: string[]) => unknown) => {
-    // messages.some
-    [].some((message: string) => {
-      if (regex) return regex.test(message);
-      return words.some((word) => {
-        if (cb) return cb(word);
-        return message.includes(word);
-      });
-    });
-  };
-};
+const npmBump = async () => {};
+
+export const bumpGitHubPackage = async () => {};
+
+export const commitBump = async () => {};
 
 export default {
   getPackage,
-  messagesSome,
+  bumpGitHubPackage,
+  npmBump,
 };
