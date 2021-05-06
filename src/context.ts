b59c7abe5f053e38e450ab927dd1b9f25f844391
wormjim <gcooled29@gmail.com>
2021-05-06 16:35:04 -0400

    Fix: Remove Default Imports In Context
    The action will pass default values which were set in the action.yml file.  No need to use a defaults object here.

--- src/context.ts
+++ src/context.ts
@@ -7,29 +7,20 @@ export interface Inputs {
   tag: boolean;
   minor: string[];
   major: string[];
-  patch: string[] | undefined;
+  patch: string[];
   ref: string;
 }
 
 export const getInputs = async (): Promise<Inputs> => {
-  const defaults = {
-    commitMessage: 'CI: Bump version to {{version}}',
-    pathToPackage: process.env.GITHUB_WORKSPACE!,
-    major: ['BREAKING CHANGE', 'major'],
-    minor: ['feature', 'minor'],
-    patch: [''],
-    ref: 'main',
-  };
-
   return {
     token: core.getInput('token', { required: true }),
-    commitMessage: core.getInput('commit_message') || defaults.commitMessage,
-    pathToPackage: core.getInput('path_to_package') || defaults.pathToPackage,
-    major: (core.getInput('major').length && [...defaults.major, ...core.getInput('major').split(',')]) || defaults.major,
-    minor: (core.getInput('minor').length && [...defaults.minor, ...core.getInput('minor').split(',')]) || defaults.minor,
-    patch: (core.getInput('patch').length && [...defaults.patch, ...core.getInput('patch').split(',')]) || undefined,
+    commitMessage: core.getInput('commit_message'),
+    pathToPackage: core.getInput('path_to_package'),
+    major: core.getInput('major').split(','),
+    minor: core.getInput('minor').split(','),
+    patch: core.getInput('patch').split(','),
     tag: /true/i.test(core.getInput('tag')),
-    ref: core.getInput('ref').split('/').pop() || defaults.ref,
+    ref: core.getInput('ref')!.split('/')!.pop()!,
   };
 };
 
