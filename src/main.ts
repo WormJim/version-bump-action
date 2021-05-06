7bdd5a3d3f8f99347b3d7e2274269ff75de6fa50
wormjim <gcooled29@gmail.com>
2021-05-06 11:42:46 -0400

    Try Again

--- src/main.ts
+++ src/main.ts
@@ -43,8 +43,9 @@ async function run(): Promise<void> {
 
     // Resolve Current Release Version From Package Json
     // const pkgVersion = (await getPackage(inputs.pathToPackage)).version.toString();
-    console.log(`core.getInput('path-to-package')`, core.getInput('path-to-package'));
-    const { version: pkgVersion } = await import(path.join(core.getInput('path-to-package'), 'package.json'));
+    // console.log(`core.getInput('path-to-package')`, core.getInput('path-to-package'));
+    console.log('Resolved Path: ', path.resolve('../', 'package.json'));
+    const { version: pkgVersion } = await import(path.resolve('../', 'package.json'));
     core.info(`Current Version is: ${pkgVersion}`);
 
     // Bump Runner Package Json
