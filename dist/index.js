aab25b6abf3a8846104c53497d9f896c0c5739b6
wormjim <gcooled29@gmail.com>
2021-05-06 09:34:22 -0400

    Use Workspace For Package Resolve

--- dist/index.js
+++ dist/index.js
@@ -7456,7 +7456,8 @@ var utils_awaiter = (undefined && undefined.__awaiter) || function (thisArg, _ar
 
 
 const getPackage = (workspace) => {
-    const pathToPackage = external_path_.join(workspace, 'package.json');
+    // const pathToPackage = path.join(workspace, 'package.json');
+    const pathToPackage = __nccwpck_require__.ab + "version-bump-action/" + process.env.GITHUB_WORKSPACE + '//package.json';
     if (!external_fs_.existsSync(pathToPackage))
         throw new Error(`${pathToPackage} could not be found in your project's root.`);
     return __nccwpck_require__(875)(pathToPackage);
--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export const getPackage = (workspace: string): { version: string } => {
-  const pathToPackage = path.join(workspace, 'package.json');
+  // const pathToPackage = path.join(workspace, 'package.json');
+  const pathToPackage = path.resolve(process.env.GITHUB_WORKSPACE!, 'package.json');
   if (!fs.existsSync(pathToPackage)) throw new Error(`${pathToPackage} could not be found in your project's root.`);
   return require(pathToPackage);
 };
