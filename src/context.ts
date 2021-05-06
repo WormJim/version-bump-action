594c98b276aa130804d0f05fe002ff65c3a94047
wormjim <gcooled29@gmail.com>
2021-05-06 16:46:54 -0400

    Fix: Revert Changes In Context Inputs
    Caused pushed not ver verison. Reverting resolves this.
    
    Until a solution is found a ticket should be made.

--- src/context.ts
+++ src/context.ts
@@ -7,20 +7,29 @@ export interface Inputs {
   tag: boolean;
   minor: string[];
   major: string[];
-  patch: string[];
+  patch: string[] | undefined;
   ref: string;
 }
 
 export const getInputs = async (): Promise<Inputs> => {
+  const defaults = {
+    commitMessage: 'CI: Bump version to {{version}}',
+    pathToPackage: process.env.GITHUB_WORKSPACE!,
+    major: ['BREAKING CHANGE', 'major'],
+    minor: ['feature', 'minor'],
+    patch: [''],
+    ref: 'main',
+  };
+
   return {
     token: core.getInput('token', { required: true }),
-    commitMessage: core.getInput('commit_message'),
-    pathToPackage: core.getInput('path_to_package'),
-    major: core.getInput('major').split(','),
-    minor: core.getInput('minor').split(','),
-    patch: core.getInput('patch').split(','),
+    commitMessage: core.getInput('commit_message') || defaults.commitMessage,
+    pathToPackage: core.getInput('path_to_package') || defaults.pathToPackage,
+    major: (core.getInput('major').length && [...defaults.major, ...core.getInput('major').split(',')]) || defaults.major,
+    minor: (core.getInput('minor').length && [...defaults.minor, ...core.getInput('minor').split(',')]) || defaults.minor,
+    patch: (core.getInput('patch').length && [...defaults.patch, ...core.getInput('patch').split(',')]) || undefined,
     tag: /true/i.test(core.getInput('tag')),
-    ref: core.getInput('ref')!.split('/')!.pop()!,
+    ref: core.getInput('ref').split('/').pop() || defaults.ref,
   };
 };
 
