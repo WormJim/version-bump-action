9986cf54d06c595bfcb61d62acc80b3887859bbb
wormjim <gcooled29@gmail.com>
2021-05-06 09:48:31 -0400

    Build Output

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,8 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
-export const getPackage = (workspace: string = '../'): { version: string } => {
+export const getPackage = (workspace: string): { version: string } => {
   const pathToPackage = path.join('../', 'package.json');
-  // const pathToPackage = path.resolve(, 'package.json');
   if (!fs.existsSync(pathToPackage)) throw new Error(`${pathToPackage} could not be found in your project's root.`);
   return require(pathToPackage);
 };
