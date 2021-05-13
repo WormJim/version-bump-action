3ea253e331d88d5c9e022a0a789e3c6ef9c39e45
wormjim <gcooled29@gmail.com>
2021-05-13 00:17:47 -0400

    Set Inputs To Const Variable Outside Returned Obj

--- src/context.ts
+++ src/context.ts
@@ -35,16 +35,26 @@ export const getInputs = async (): Promise<Inputs> => {
     ref: 'main',
   };
 
+  const token = coreInput('token', true);
+  const commitMessage = coreInput('commit_message') || defaults.commitMessage;
+  const pathToPackage = coreInput('path_to_package') || defaults.pathToPackage;
+  const major = (coreInput('major').length && [...defaults.major, ...coreInput('major').split(',')]) || defaults.major;
+  const minor = (coreInput('minor').length && [...defaults.minor, ...coreInput('minor').split(',')]) || defaults.minor;
+  const patch = (coreInput('patch').length && [...defaults.patch, ...coreInput('patch').split(',')]) || undefined;
+  const tag = /true/i.test(coreInput('tag'));
+  const ref = coreInput('ref').split('/').pop() || defaults.ref;
+  const bump = /false/i.test(boolConvert(coreInput('bump'))) ? false : coreInput('bump');
+
   return {
-    token: coreInput('token', true),
-    commitMessage: coreInput('commit_message') || defaults.commitMessage,
-    pathToPackage: coreInput('path_to_package') || defaults.pathToPackage,
-    major: (coreInput('major').length && [...defaults.major, ...coreInput('major').split(',')]) || defaults.major,
-    minor: (coreInput('minor').length && [...defaults.minor, ...coreInput('minor').split(',')]) || defaults.minor,
-    patch: (coreInput('patch').length && [...defaults.patch, ...coreInput('patch').split(',')]) || undefined,
-    tag: /true/i.test(coreInput('tag')),
-    ref: coreInput('ref').split('/').pop() || defaults.ref,
-    bump: /false/i.test(boolConvert(coreInput('bump'))) ? false : coreInput('bump'),
+    token,
+    commitMessage,
+    pathToPackage,
+    major,
+    minor,
+    patch,
+    tag,
+    ref,
+    bump,
   };
 };
 
